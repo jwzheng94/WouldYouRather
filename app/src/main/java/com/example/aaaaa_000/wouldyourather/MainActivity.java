e0150292e609c568fafdd84ae6b78887dcc1f171
Jason Zheng <jwzheng94@gmail.com>
2015-10-10 16:52:19 -0700

    Trying to Fix Everything

--- app/src/main/java/com/example/aaaaa_000/wouldyourather/MainActivity.java
+++ app/src/main/java/com/example/aaaaa_000/wouldyourather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aaaaa_000.wouldyourather;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +29,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button ask = (Button) findViewById(R.id.ask_button);
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AskQuestionActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
