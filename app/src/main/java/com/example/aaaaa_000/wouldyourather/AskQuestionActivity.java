39b2aa3fd7bec31e3de784b386051748182cc587
theyingbin <yingbinw@yahoo.com>
2015-10-13 17:18:43 -0700

    Empty String filtering

--- app/src/main/java/com/example/aaaaa_000/wouldyourather/AskQuestionActivity.java
+++ app/src/main/java/com/example/aaaaa_000/wouldyourather/AskQuestionActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.parse.ParseObject;
 
@@ -29,10 +30,41 @@ public class AskQuestionActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), AskResultActivity.class);
-                startActivity(i);
+
+                Boolean noErrors = true;
+                EditText text1 = (EditText) findViewById(R.id.answer1);
+                EditText text2 = (EditText) findViewById(R.id.answer2);
+
+                String answer1 = text1.getText().toString();
+                String answer2 = text2.getText().toString();
+
+                if(!filter(answer1)) {
+                    noErrors = false;
+                    text1.setError("Field is empty");
+                }
+                if(!filter(answer2)) {
+                    noErrors = false;
+                    text2.setError("Field is empty");
+                }
+
+                if(noErrors){
+                    ParseObject question = new ParseObject("Questions");
+                    question.put("firstOption", answer1);
+                    question.put("secondOption", answer2);
+                    question.put("countFirst", 0);
+                    question.put("countSecond", 0);
+                    question.saveInBackground();
+
+                    Intent i = new Intent(getApplicationContext(), AskResultActivity.class);
+                    startActivity(i);
+                }
+
             }
         });
+    }
+
+    public static boolean filter(String word){
+        return !word.equals("");
     }
 
 }
